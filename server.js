6ea61f2fcb50d00f9a545617ededea951a734526
br100k <bermuda.ctrl@gmail.com>
2026-03-01 12:15:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ app.use(express.json());
 let victims = [];
 
 app.get('/', (req, res) => {
-  res.send('🫀 Бермудский треугольник работает!');
+    res.send('🫀 Бермудский треугольник работает!');
 });
 
 app.get('/api/victims', (req, res) => {
-  res.json(victims);
+    res.json(victims);
 });
 
 app.post('/api/data', (req, res) => {
-  const data = req.body;
-  data.timestamp = Date.now();
-  victims.push(data);
-  res.json({ status: 'ok' });
+    const data = req.body;
+    data.timestamp = Date.now();
+    victims.push(data);
+    res.json({ status: 'ok' });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🫀 Сервер запущено на порту ${PORT}`);
-}); 
+    console.log(`🫀 Сервер запущен на порту ${PORT}`);
+});
