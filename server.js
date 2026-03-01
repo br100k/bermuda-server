e4c27f21e8897f435eacd88b83c2af753025f497
br100k <bermuda.ctrl@gmail.com>
2026-03-01 15:25:16 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let victims = [];
+
+app.get('/', (req, res) => {
+    res.send('Бермудский треугольник работает!');
+});
+
+app.get('/api/victims', (req, res) => {
+    res.json(victims);
+});
+
+app.post('/api/data', (req, res) => {
+    const data = req.body;
+    data.timestamp = Date.now();
+    victims.push(data);
+    res.json({ status: 'ok' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Сервер запущен!');
+});
